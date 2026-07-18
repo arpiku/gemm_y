d044a9c42c6de4e869e28146fcb8c0e5b1a9a0e3
arpiku <akphysics03@gmail.com>
2026-07-18 14:17:30 +0530

    Improved tracer

--- src/Tracer.h
+++ src/Tracer.h
@@ -1,45 +1,66 @@
 #pragma once
 
-#include<iostream>
-#include<chrono>
-#include <ratio>
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <ostream>
+#include <span>
+#include <string_view>
 
-namespace Tracer {
-    using hres_clk = std::chrono::high_resolution_clock;
-    using time_pt = hres_clk::time_point;
-    using nano_sec = std::chrono::nanoseconds;
-    using micro_sec = std::chrono::microseconds;
-    using milli_sec = std::chrono::milliseconds;
+namespace tracer {
 
-    enum Units {MILLI, MICRO, NANO};
+using clock = std::chrono::steady_clock;
+using time_point = clock::time_point;
 
-    struct Event {
-        std::string_view label;
-        time_pt tp;
-    };
-
-    template<std::size_t Capacity = 128>
-    class Timer {
-        public:
-            Timer() : count_(0) {
-                event_markers_[count_] = Event{"__Origin__", hres_clk::now()};
-            }
-            void mark(std::string_view = "^"); // Random "^" symbol for marking without names
-            void print_traces(); // Print out collected markers list, with time stamp as is
-            void print_traces_delta(); // Print out collected markers list, with delta w.r.t previous marker
-            void print_delta_events(std::size_t event_0_idx, std::size_t event_1_idx); // To print delta between given event indices
-
-        private:
-            std::array<Event,Capacity> event_markers_;
-            std::size_t count_;
-    };
-
-    class Zone_Timer {
-        public:
-            Zone_Timer(Units unit) : start_(hres_clk::now()), u_(unit) {}
-            ~Zone_Timer() {}
-        private:
-            time_pt start_;
-            Units u_;
-    };
+struct Event {
+    std::string_view label;
+    time_point timestamp;
 };
+
+template<std::size_t Capacity = 128>
+class Timer {
+    static_assert(Capacity > 0);
+
+public:
+    Timer() noexcept {
+        event_markers_[0] = {"__Origin__", clock::now()};
+        count_ = 1;
+    }
+
+    [[nodiscard]]
+    bool mark(std::string_view label = "^") noexcept {
+        if (count_ == Capacity) {
+            ++dropped_events_;
+            return false;
+        }
+
+        event_markers_[count_++] = {label, clock::now()};
+        return true;
+    }
+
+    void reset() noexcept {
+        count_ = 1;
+        dropped_events_ = 0;
+        event_markers_[0] = {"__Origin__", clock::now()};
+    }
+
+    [[nodiscard]]
+    std::span<const Event> events() const noexcept {
+        return {event_markers_.data(), count_};
+    }
+
+    [[nodiscard]]
+    std::size_t dropped_events() const noexcept {
+        return dropped_events_;
+    }
+
+    void print_traces(std::ostream& out) const;
+    void print_traces_delta(std::ostream& out) const;
+
+private:
+    std::array<Event, Capacity> event_markers_{};
+    std::size_t count_ = 0;
+    std::size_t dropped_events_ = 0;
+};
+
+} // namespace tracer
